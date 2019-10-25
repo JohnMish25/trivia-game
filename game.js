d818762bf46b69615fda91145d99d76eed0c03d8
John Mish <johnmish25@gmail.com>
2019-10-24 23:39:41 -0400

    adding new .html and .js

--- game.js
+++ game.js
@@ -1,72 +1,13 @@
-//List All Variables
-//Questions variables
+$(document).ready(function() {
 
-var questions=[{
-    q: "Which of the following Dunder Mifflin employess does not work in the Accounting Dept.?",
-    a1: "Oscar Martinez"
-    a2: "Angela Martin"
-    a3: "Toby Flenderson"
-    a4: "Kevin Malone"
-    correct: "Toby Flenderson"
-},
 
-{
-    q: "Where do Pam & Jim get married?"
-    a1: "Las Vegas"
-    a2: "Scranton"
-    a3: "Niagra Falls"
-    a4: "Philadelphia"
-    correct: "Niagra Falls"
-}
-{
-    q: "Which of the following, IS NOT one of Dwight Shrute's favorite hobbies/interests?"
-    a1. "Beets"
-    a2: "Bears"
-    a3: "Battlestar Galactica"
-    a4: "Softball"
-    correct: "softball"
-}
-{
-    q: "Unsatisfied with her current role of secrety, Pam aspires to be a(n) _____"
-    a1: "Artist"
-    a2: "Teacher"
-    a3: "Web Developer"
-    a4: "Lawyer"
-    correct: "Artist"
+    
+  $(".questionsSection").hide();
+ 
+    $("#gameStart").on('click', function(){
+        $(".questionsSection").show();
+        $(".open").hide();
+    }
 
-}
-{
-    q: "Michael hit this Dunder Mifflin employee with his car"
-   a1: "Toby Flenderson"
-    a2: "Jim Halpert"
-    a3: "Meredith Palmer"
-    a4: "Creed Bratton"
-    correct: "Meredith Palmer"
-}
-{
-    q: "Which of these Dunder Mifflin employees is unsure of his/her actual job title?"
-    a1: "Michael Scott"
-    a2: "Creed Bratton"
-    a3: "Kelly Kapoor"
-    a4: "Erin Hannon"
-    correct: "Creed Bratton"
-}
-{
-    q: "After quitting Dunder Mifflin, what new company does Michael Scott create?"
-    a1: "Shoe La La"
-    a2: "The Michael Scottt Paper Company"
-    a3: "Dunder Mifflin Infinity"
-    a4: "Sabre"
-    correct: "The Michael Scottt Paper Company"
 
-}
-{
-    q: "After transferring to the Scranton branch, which employee is forced to go anger management classes?"
-    a1: "Andy Bernard"
-    a2: "Karen Filipelli"
-    a3: "Gabe Lewis"
-    a4: "Deangelo Vickers"
-    correct: "Andy Bernard"
-}
-
-]
+    );})
