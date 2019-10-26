1dfe65e6187b013d12bf6ac591c5f44e204dc35d
John Mish <johnmish25@gmail.com>
2019-10-25 20:02:21 -0400

    updated .html

--- game.js
+++ game.js
@@ -1,57 +1,60 @@
 $(document).ready(function() {
 
-var seconds = 60; 
-console.log (seconds)
-var intervalId = 0;
-console.log (intervalId)
-var correctAnswers = 1;
-console.log (correctAnswers)
-var incorrectAnswers = 2;
-console.log (incorrectAnswers)
-
-
-$("#numCorrectAnswers").text(correctAnswers);
-$("#numIncorrectAnswers").text(incorrectAnswers);
-
  
+  var correctAnswers = 0;
+  
+  var incorrectAnswers = 0;
 
   
-
-//events that will affect what shows on the screen for the user. 
-
-//game begins with a start screen, the questions, results & timer are hidden
-
-$(".questionsSection").hide();
-  $(".resultsPage").hide();
-
-
- 
+  
+  $("#numCorrectAnswers").text(correctAnswers);
+  console.log (correctAnswers)
+  $("#numIncorrectAnswers").text(incorrectAnswers);
+  console.log (incorrectAnswers)
+  
    
- 
-
-  //click event, will trigger the game to begin 
-    $("#gameStart").on('click', function(){
-        $(".resultsPage").hide();
-        $(".questionsSection").show();
-        $(".open").hide();
-        
-    })
-
-  //end of game. user clicks to see their results   
-    $("#submitBtn").on('click', function(){
-        $(".resultsPage").show();
-        $(".questionsSection").hide();
-        $(".open").hide();
-        
-    })
-    //play again takes the user back to the start screen
-    $("#playAgain").on('click', function(){
-        $(".resultsPage").hide();
-        $(".questionsSection").hide();
-        $(".open").show();
-        
-    })
-
   
+    
+  
+  //events that will affect what shows on the screen for the user. 
+  
+  //game begins with a start screen, the questions, results & timer are hidden
+  
+  $(".questionsSection").hide();
+    $(".resultsPage").hide();
+  
+  
+   
+     
+   
+  
+    //click event, will trigger the game to begin 
+      $("#gameStart").on('click', function(){
+          $(".resultsPage").hide();
+          $(".questionsSection").show();
+          $(".open").hide();
+          
+      })
+  
+    //end of game. user clicks to see their results   
+      $("#submitBtn").on('click', function(){
+          $(".resultsPage").show();
+          $(".questionsSection").hide();
+          $(".open").hide();
+          
+      })
+      //play again takes the user back to the start screen
+      $("#playAgain").on('click', function(){
+          $(".resultsPage").hide();
+          $(".questionsSection").hide();
+          $(".open").show();
+          
+      })
 
-  });
+
+
+      
+        });
+  
+    
+  
